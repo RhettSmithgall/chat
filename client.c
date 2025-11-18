72035737b9c36105a8689ad5f32344c6342201ea
Rhett Smithgall <rhettsmithgall@gmail.com>
2025-11-18 08:54:59 -0500

    started chat!

---
+++ client.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc,char* argv[]) {
+    
+    if(server){ //if no response from server, make one
+        //make server
+    }
+
+    //if run with an argument, connect to argument user
+    if(argc > 0){
+        //send a connect request to ./chat <user>
+    }
+
+    //else
+
+    //connect to server and to active pool
+
+    //send to community board
+
+    //send chat request to another user
+
+    //accept chat request
+}
---
+++ server.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    //main loop
+    while(){
+        //listen for clients
+
+        if() //when a client connects, add them to the active pool
+
+        fork()//maybe active pool needs to be its own proccess?
+    }
+
+    return 0;
+}
+
+void activePool(){
+    //when one user sends a chat request to another, pass it along
+    //when the other user accepts, give them a port to chat along and connect them
+}
+
+void communityBoard(){
+    //a community chat board people in the active pool can add to
+}
+
+
+
